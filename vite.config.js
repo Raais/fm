c3d2f8e6122072b7cb01c264e9ab47331234e1f0
Raais <raais@pm.me>
2024-07-29 23:22:15 +0500

    polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from "vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [nodePolyfills(),tsconfigPaths(),react()],
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
     },
     host: "0.0.0.0",
-    base: '/fm/',
   },
   base: '/fm/',
   optimizeDeps: {
